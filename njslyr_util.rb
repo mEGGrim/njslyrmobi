30591bb47e2fe022ca5417b9687885f7d645f3d1
nomura <nomura@debian>
2014-06-12 16:48:03 +0900

    modified

--- njslyr_util.rb
+++ njslyr_util.rb
@@ -13,19 +13,25 @@ module NJSLYR_Util
 	end
 
 	# 引数として渡したファイルの結合を行う
-	def join_episodes(firstfile)
-		puts episodetitle = "#{File.basename(firstfile).gsub(/(\..*)|\s|(#[0-9]+)/, '')}"
-		files = Dir.glob("#{File.dirname firstfile}/#{episodetitle}*").sort
+	def join_episodes(first_file)
+		episode_title = File.basename(first_file).notNumberedTitle
+		files = Dir.glob("#{File.dirname first_file}/#{episode_title}*").sort
 		if !files.empty?
 			arr = []
 			files.each do |file|
 				arr.push File.open(file).read
 			end
-			joined_path = File.expand_path "#{episodetitle}.txt"
+			joined_path = File.expand_path "#{episode_title}.txt"
 			File.open(joined_path, 'w').write arr.join("\n")
 			puts "JOINED: #{joined_path}"
 		end
 	end
 
 	module_function :get_episodes, :join_episodes
+end
+
+class String
+	def notNumberedTitle
+		return self.gsub(/(#[0-9]+)|(\.txt)|\s/, '')
+	end
 end
